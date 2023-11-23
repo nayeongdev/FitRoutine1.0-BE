ad8cfbb49a883546c376e61bdeb24b2b92dc7194
nayeongdev <nayeongkim.dev@gmail.com>
2023-11-23 13:18:49 +0900

    :fire: Remove maker files
    maker 파일을 지우고 정리, maker 기능은 routines에 넣을 계획

--- ___/settings.py
+++ ___/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     "rest_framework",
     # custom
     'accounts',
-    'maker',
     'routines',
 ]
 
--- ___/urls.py
+++ ___/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("maker/", include("maker.urls")),
     path("routines/", include("routines.urls")),
 ]
