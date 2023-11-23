2d9b380a7efc5fb81075333e666c9af0fd26c023
nayeongdev <nayeongkim.dev@gmail.com>
2023-11-23 22:21:49 +0900

    :sparkles: Feat: 커스텀 유저 모델 생성 (accounts)
    확장된 유저 모델 생성
    1. username 대신 email로 로그인
    2. 개인정보 속성 추가(성별, 생년월일, 키, 몸무게)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+GENDER_CHOICES = (
+    ("남자", "남자"),
+    ("여자", "여자"),
+)
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = CustomUserManager()
+
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, blank=True, null=True
+    )
+    date_of_birth = models.DateField(blank=True, null=True)
+    height = models.FloatField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email
