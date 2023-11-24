db58e8e5f75c9b91ab32456e89d1b371e37a545b
nayeongdev <nayeongkim.dev@gmail.com>
2023-11-24 23:58:54 +0900

    :sparkles: Feat: 커스텀 유저 모델 수정 (accounts)
    1. CustomUser 모델에 JWT를 담기위한 함수 추가
    2. managers.py 정리

--- accounts/managers.py
+++ accounts/managers.py
@@ -4,8 +4,12 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError(_("The Email must be set"))
+        """Create and return a `User` with an email, username and password."""
+        if username is None:
+            raise TypeError(_("Users must have a username."))
+
+        if email is None:
+            raise TypeError(_("Users must have an email address."))
 
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
@@ -14,14 +18,13 @@ class CustomUserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, username, email, password, **extra_fields):
+        """Create and return a `User` with superuser (admin) permissions."""
+        if password is None:
+            raise TypeError(_("Superusers must have a password."))
+        
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-        
         return self.create_user(username, email, password, **extra_fields)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .managers import CustomUserManager
 
@@ -10,21 +12,29 @@ GENDER_CHOICES = (
 )
 
 
-class CustomUser(AbstractUser):
-    username = models.CharField(max_length=255, unique=True)
+class CustomUser(AbstractUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
     email = models.EmailField(db_index=True, unique=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
-
-    objects = CustomUserManager()
-
     gender = models.CharField(
         max_length=10, choices=GENDER_CHOICES, blank=True, null=True
     )
     date_of_birth = models.DateField(blank=True, null=True)
     height = models.FloatField(blank=True, null=True)
     weight = models.FloatField(blank=True, null=True)
+    first_name = None
+    last_name = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.email
+        return f"{self.username}({self.email})"
+
+    @property
+    def tokens(self) -> dict[str, str]:
+        """Allow us to get a user's token by calling `user.token`."""
+        refresh = RefreshToken.for_user(self)
+        return {"refresh": str(refresh), "access": str(refresh.access_token)}
